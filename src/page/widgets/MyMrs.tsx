313cbe3283e1cff58d14bb0aecaa418a2228b1ba
codethread <adamhalldesigns@gmail.com>
2025-04-12 20:37:20 +0100

    shows on dashboard

--- src/page/widgets/MyMrs.tsx
+++ src/page/widgets/MyMrs.tsx
@@ -5,6 +5,7 @@ import {Button} from "@/components/ui/button"
 import {Card, CardHeader, CardTitle, CardContent} from "@/components/ui/card"
 import {Separator} from "@/components/ui/separator"
 import {Lead} from "@/components/ui/text"
+import type {MrFragmentFragment} from "@/graphql/graphql"
 import {useConfigSelector} from "@/hooks/config/useConfig"
 import {useFetcher} from "@/hooks/fetcher/useFetcher"
 import {duration} from "@/lib/duration"
@@ -39,76 +40,35 @@ export function MyMrs() {
 		.filter(uniqueBy("id"))
 
 	return (
-		<div className="@container w-[100%] flex-1">
-			<div className="gap-sm @min-5xl:gap-lg flex flex-wrap items-stretch justify-center">
-				<Card className="max-w-[350px] flex-1 basis-[280px]">
-					<CardHeader>
-						<div className="flex justify-between">
-							<CardTitle>
-								<Lead>total {mrs?.length}</Lead>
-								<p className="text-muted-foreground text-xs">
-									Last fetch: {format(dataUpdatedAt, "eee hh:mm:ss")}
-								</p>
-								<p className="text-muted-foreground text-xs">
-									<NextFetch dataUpdatedAt={dataUpdatedAt} />
-								</p>
-							</CardTitle>
-							<Button
-								variant={"outline"}
-								size="iconSm"
-								onClick={() => refetch()}
-							>
-								<RefreshCw className={isFetching ? "animate-spin" : ""} />
-							</Button>
-						</div>
-					</CardHeader>
-					<CardContent>
-						<p>
-							you have{" "}
-							<a
-								target="_blank"
-								href={`https://${domain}/dashboard/merge_requests?assignee_username=${user}`}
-								rel="noreferrer"
-							>
-								{assignedMergeRequests?.count} open MRs
-							</a>
-						</p>
-						<Separator />
-						<ul>
-							{mrs?.map((mr) => (
-								<li key={mr?.id}>
-									<div>
-										<p className="truncate">
-											{mr?.webUrl && (
-												<a href={mr?.webUrl} target="_blank" rel="noreferrer">
-													{mr?.title}
-												</a>
-											)}
-											{/* {mr?.mergeable ? ( */}
-											{/* 	<span> */}
-											{/* 		<Check color="green" /> mergeable */}
-											{/* 	</span> */}
-											{/* ) : null} */}
-										</p>
-										{/* {mr?.mergeable ? null : ( */}
-										{/* 	<ul> */}
-										{/* 		{mr?.conflicts ? <li>❌ conflicts</li> : null} */}
-										{/* 		{mr?.headPipeline?.status ? ( */}
-										{/* 			<li>Pipeline: {mr.headPipeline.status}</li> */}
-										{/* 		) : null} */}
-										{/* 		{mr?.approved ? <li>√ approved</li> : null} */}
-										{/* 		{mr?.approvalState.invalidApproversRules */}
-										{/* 			?.filter((r) => !r.allowMergeWhenInvalid) */}
-										{/* 			.map((r) => r.name) */}
-										{/* 			.join(" ") ?? null} */}
-										{/* 	</ul> */}
-										{/* )} */}
-									</div>
-								</li>
-							))}
-						</ul>
-					</CardContent>
-				</Card>
+		<div className="@container">
+			<div className="gap-sm @min-5xl:gap-lg flex flex-col items-stretch justify-center">
+				<Lead>total {mrs?.length}</Lead>
+				<Button variant={"outline"} size="iconSm" onClick={() => refetch()}>
+					<RefreshCw className={isFetching ? "animate-spin" : ""} />
+				</Button>
+				<p className="text-muted-foreground text-xs">
+					Last fetch: {format(dataUpdatedAt, "eee hh:mm:ss")}
+				</p>
+				<p className="text-muted-foreground text-xs">
+					Next fetch: <NextFetch dataUpdatedAt={dataUpdatedAt} />
+				</p>
+				<p>
+					you have{" "}
+					<a
+						target="_blank"
+						href={`https://${domain}/dashboard/merge_requests?assignee_username=${user}`}
+						rel="noreferrer"
+					>
+						{assignedMergeRequests?.count} open MRs
+					</a>
+				</p>
+				<ul>
+					{mrs?.filter(Boolean).map((mr) => (
+						<li key={mr?.id}>
+							<Mr mr={mr} />
+						</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	)
@@ -128,13 +88,54 @@ function NextFetch({dataUpdatedAt}: {dataUpdatedAt: number}) {
 		return () => clearInterval(i)
 	}, [])
 
+	const secondsToUpdate = Math.floor(
+		(dataUpdatedAt + duration(amount, unit) - now) / 1000,
+	)
+
+	if (secondsToUpdate <= 0 || secondsToUpdate >= duration(amount, unit))
+		return null
+
 	return (
-		<span>
-			Next fetch:{" "}
-			{rtf1.format(
-				Math.floor((dataUpdatedAt + duration(amount, unit) - now) / 1000),
-				"second",
-			)}
+		<span className="tabular-nums">
+			{rtf1.format(secondsToUpdate, "second")}
 		</span>
+	)
+}
+
+function Mr({mr}: {mr: MrFragmentFragment}) {
+	return (
+		<Card className="flex-1">
+			<CardHeader>
+				<CardTitle>
+					<p className="truncate">
+						{mr?.webUrl && (
+							<a href={mr?.webUrl} target="_blank" rel="noreferrer">
+								{mr?.title}
+							</a>
+						)}
+						{mr?.mergeable ? (
+							<span>
+								<Check className="inline-flex" color="green" /> mergeable
+							</span>
+						) : null}
+					</p>
+				</CardTitle>
+			</CardHeader>
+			<CardContent>
+				{mr?.mergeable ? null : (
+					<ul>
+						{mr?.conflicts ? <li>❌ conflicts</li> : null}
+						{mr?.headPipeline?.status ? (
+							<li>Pipeline: {mr.headPipeline.status}</li>
+						) : null}
+						{mr?.approved ? <li>√ approved</li> : null}
+						{mr?.approvalState.invalidApproversRules
+							?.filter((r) => !r.allowMergeWhenInvalid)
+							.map((r) => r.name)
+							.join(" ") ?? null}
+					</ul>
+				)}
+			</CardContent>
+		</Card>
 	)
 }
